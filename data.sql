376513a931487619654d003d864e569e56531c14
Oyewunmi Kelvin Gbenga <kelvinoye@gmail.com>
2022-08-30 10:27:59 +0100

    resolve p_key constraint conflict in vists

--- data.sql
+++ data.sql
@@ -95,3 +95,15 @@ INSERT INTO visits (animals_id, vets_id, date_of_visit)
 	VALUES(10,3,'2020-05-24');
 INSERT INTO visits (animals_id, vets_id, date_of_visit)
 	VALUES(10,1,'2021-01-11');
+
+-- performance audit
+
+-- This will add 3.594.280 visits considering you have 10 animals, 4 vets, and it will use around ~87.000 timestamps (~4min approx.)
+INSERT INTO visits (animals_id, vets_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animal_id, (SELECT id FROM vets) vets_id, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+-- This will add 2.500.000 owners with full_name = 'Owner <X>' and email = 'owner_<X>@email.com' (~2min approx.)
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
+ALTER TABLE visits DROP CONSTRAINT visits_pkey;
